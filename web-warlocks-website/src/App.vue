4303b4afd5924e8c0a2ca138b61e1e078e417c6c
Tyler <tyler.dins88@gmail.com>
2024-03-17 16:24:46 -0700

    install fontawesome. imported icon. home rundown switched over.

--- web-warlocks-website/src/App.vue
+++ web-warlocks-website/src/App.vue
@@ -6,9 +6,11 @@
       <div class="greetings">
         <h1 class="green">Welcome to the Good Stuff.</h1>
         <h3>
-          You’ve successfully created a project with
+          Your go to coders for apps and websites built with:
           <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-          <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+          <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a> or
+          <a href="https://create-react-app.dev/" target="_blank" rel="noopener">React</a> or
+          <a href="https://www.squarespace.com/" target="_blank" rel="noopener">SquareSpace</a>.
         </h3>
       </div>
 
@@ -40,6 +42,14 @@ header {
   margin: 0 auto 2rem;
   border-top-left-radius: 50px;
   border-bottom-right-radius: 50px;
+  box-shadow:
+    0 0 20px 0 rgba(255, 87, 34, 0.7),
+    0 0 40px 0 rgba(255, 87, 34, 0.5),
+    0 0 60px 0 rgba(255, 87, 34, 0.3);
+  transform: scale(1.01);
+  transition:
+    box-shadow 0.3s,
+    transform 0.3s;
 }
 
 h1 {
@@ -70,7 +80,7 @@ nav a.router-link-exact-active {
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  background-color: rgba(255, 87, 34, 0.5);
 }
 
 nav a {
--- web-warlocks-website/src/components/TheHomeRundown.vue
+++ web-warlocks-website/src/components/TheHomeRundown.vue
@@ -1,88 +1,60 @@
-<script setup>
-import RundownItem from './RundownItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
-
 <template>
   <RundownItem>
     <template #icon>
-      <DocumentationIcon />
+      <WizardHatIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Projects</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    View our projects such as
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Groovana,</a>
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">The American Sommelier,</a>and
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">E-Commerce</a>here!
   </RundownItem>
 
   <RundownItem>
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>What We Offer. </template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
+    <div>
+      we offer projects built with all of the following:
 
-    <br />
+      <br />
 
-    More instructions are available in <code>README.md</code>.
+      <a href="https://flowbite-vue.com/" target="_blank" rel="noopener">Flowbite,</a>
+      <a href="https://v2.tailwindcss.com/" target="_blank" rel="noopener">Tailwind,</a>
+      <a href="https://www.npmjs.com/package/axios" target="_blank" rel="noopener">Axios,</a>and
+      <a href="https://aws.amazon.com/" target="_blank" rel="noopener">Amazon Web Services</a>.
+    </div>
   </RundownItem>
 
   <RundownItem>
     <template #icon>
       <EcosystemIcon />
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading>Contact Us</template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    Get in touch with us here.
   </RundownItem>
 
   <RundownItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Community</template>
+    <template #heading>Who We Are</template>
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </RundownItem>
-
-  <RundownItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Learn About Us Here.
   </RundownItem>
 </template>
+
+<script setup>
+import RundownItem from './RundownItem.vue'
+
+import WizardHatIcon from './icons/IconWizard.vue'
+
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+</script>
