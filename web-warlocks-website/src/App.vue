ac589f3b75de1cd1ef3569df3be1def0b5856854
Doomtower <jessehudak707@outlook.com>
2024-03-19 13:40:27 -0700

    created custom navbar component, cleaning up app.vue

--- web-warlocks-website/src/App.vue
+++ web-warlocks-website/src/App.vue
@@ -15,22 +15,17 @@
         </h3>
       </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/projects">Projects</RouterLink>
-        <RouterLink to="/offer">Offerings</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
-        <RouterLink to="/about">About Us</RouterLink>
-      </nav>
     </div>
   </header>
-
+  <CustomNavbar />
   <RouterView />
 </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {  RouterView } from 'vue-router'; //RouterLink,
+import CustomNavbar from './components/CustomNavbar.vue';
+
 </script>
 
 <style scoped>
@@ -72,61 +67,4 @@ h3 {
 .greetings h3 {
   text-align: center;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: rgba(255, 87, 34, 0.5);
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-/* @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .webWarlock {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-} */
 </style>
---
+++ web-warlocks-website/src/components/CustomNavbar.vue
@@ -0,0 +1,62 @@
+
+<template>
+    <nav>
+      <router-link to="/">
+        <a :class="{ 'router-link-exact-active': isActive('/') }">Home</a>
+      </router-link>
+      <router-link to="/projects">
+        <a :class="{ 'router-link-exact-active': isActive('/projects') }">Projects</a>
+      </router-link>
+      <router-link to="/offer">
+        <a :class="{ 'router-link-exact-active': isActive('/offer') }">Offerings</a>
+      </router-link>
+      <router-link to="/contact">
+        <a :class="{ 'router-link-exact-active': isActive('/contact') }">Contact</a>
+      </router-link>
+      <router-link to="/about">
+        <a :class="{ 'router-link-exact-active': isActive('/about') }">About Us</a>
+      </router-link>
+    </nav>
+</template>
+  
+
+<script setup>
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+  
+  const isActive = (routePath) => {
+    return route.path === routePath;
+  };
+</script>
+
+
+
+<style>
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: rgba(255, 87, 34, 0.5);
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
